ac589d22b270c9711e006f84fb5d1a7c21526ded
Manoj G <manojharry1999@gmail.com>
2020-08-31 19:26:03 +0530

    Add All Files

--- crudbackend/src/main/java/com/product/crudapp/SpringBootApplication.java
+++
@@ -1,2 +0,0 @@
-package com.product.crudapp;public @interface SpringBootApplication {
-}
--- crudbackend/src/main/java/com/product/crudapp/repo/CrudRepo.java
+++
@@ -1,9 +0,0 @@
-package com.product.crudapp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.product.crudapp.model.Product;
-
-public interface CrudRepo extends JpaRepository<Product, Double> {
- 
-}
